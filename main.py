6dcfc6eeb980aabc79a371ca67489db13d820d8a
Shubham Chopade <31898834+shubhamchopade@users.noreply.github.com>
2023-02-17 01:57:26 -0500

    Optimized number of streams

--- main.py
+++ main.py
@@ -9,6 +9,8 @@ import numpy as np
 from json import JSONEncoder
 from helper import run_simulator, REALTIME_DATA
 import os
+import shutil
+
 
 app = Flask(__name__)
 api = Api(app)
@@ -17,18 +19,13 @@ CORS(app, resources={r"/*": {"origins": "*"}})
 
 @app.route('/run/<sessionid>', methods=['GET', 'POST'])
 def run(sessionid):
-    try:
-        file = os.mkdir(f'realtime/{sessionid}')
-    except:
-        pass
-    with open(f'realtime/{sessionid}/realtime.json', 'w') as fp:
-        pass
-    with open(f'realtime/{sessionid}/glucose_realtime.json', 'w') as fp:
-        pass
-    with open(f'realtime/{sessionid}/adenine_realtime.json', 'w') as fp:
-        pass
-    with open(f'realtime/{sessionid}/lysine_realtime.json', 'w') as fp:
-        pass
+    path = f'realtime/{sessionid}'
+    if os.path.exists(path):
+        shutil.rmtree(path)
+        os.mkdir(path)
+    else:
+        os.mkdir(path)
+
     data = run_simulator(sessionid)
     return data
 
@@ -37,69 +34,11 @@ def run(sessionid):
 def stream(sessionid):
 
     def get_data():
-        path = os.path.abspath(f'realtime/{sessionid}/realtime.json')
+        path = os.path.abspath(f'realtime/{sessionid}/realtime_updated.json')
         while path:
-            time.sleep(0.3)
-            f = open(path, "r")
-            # data = {"population": f.read()}
-            yield f'data: {f.read()}  \n\n'
-            f.close()
-    resp = Response(get_data(), mimetype='text/event-stream')
-
-    return resp
-
-
-@app.route('/<sessionid>/glucose')
-def glucose(sessionid):
-
-    def get_data():
-        glucose_path = os.path.abspath(
-            f'realtime/{sessionid}/glucose_realtime.json')
-        while glucose_path:
-            time.sleep(0.3)
-            try:
-                g = open(glucose_path, "r")
-                # data = {"glucose": g.read()}
-                yield f'data: {g.read()}  \n\n'
-                g.close()
-            except:
-                yield f'data: {""}  \n\n'
-    resp = Response(get_data(), mimetype='text/event-stream')
-
-    return resp
-
-
-@app.route('/<sessionid>/adenine')
-def adenine(sessionid):
-
-    def get_data():
-        adenine_path = os.path.abspath(
-            f'realtime/{sessionid}/adenine_realtime.json')
-        while adenine_path:
-            time.sleep(0.3)
-            g = open(adenine_path, "r")
-            # data = {"adenine": g.read()}
-            yield f'data: {g.read()}  \n\n'
-            g.close()
-    resp = Response(get_data(), mimetype='text/event-stream')
-
-    return resp
-
-
-@app.route('/<sessionid>/lysine')
-def lysine(sessionid):
-
-    def get_data():
-        # sessionid = request.args.get('sessionid')
-        # print(sessionid)
-        lysine_path = os.path.abspath(
-            f'realtime/{sessionid}/lysine_realtime.json')
-        while lysine_path:
-            time.sleep(0.3)
-            g = open(lysine_path, "r")
-            # data = {"lysine": g.read()}
-            yield f'data: {g.read()}  \n\n'
-            g.close()
+            time.sleep(1)
+            with open(path, "r") as realtime:
+                yield f'data: {realtime.read()}  \n\n'
     resp = Response(get_data(), mimetype='text/event-stream')
 
     return resp
